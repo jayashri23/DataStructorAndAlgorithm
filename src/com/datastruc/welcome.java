16ed2e848bb1ae3dc1b6d2e7fa70ff6ef6f283d4
jayashri23 <jsjankar23@gmail.com>
2022-11-13 15:19:39 +0530

    Welcome To Data Structure and Algorithm Practice Proble

---
+++ src/com/datastruc/welcome.java
@@ -0,0 +1,9 @@
+package com.datastruc;
+
+public class welcome {
+    public static void main(String [] args){
+        System.out.println(".......................................................");
+        System.out.println("Welcome ToData Structure and Algorithm Practice Problem");
+        System.out.println(".......................................................");
+    }
+}
